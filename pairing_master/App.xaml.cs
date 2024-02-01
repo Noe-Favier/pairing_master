123598eb63975b1919bf419b8802a8c81a15e9da
Noé <noe.favier@outlook.com>
2024-02-01 14:53:21 +0100

    initial commit

---
+++ pairing_master/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pairing_master;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
